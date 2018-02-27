23beea081e27528c46f1c8d044d231452645ba97
SP-CBAUTISTA-HP\Clarese Bautista <cbautista@stratpoint.com>
2018-02-27 14:24:44 +0800

    handling for invalid input

--- src/Main.kt
+++ src/Main.kt
@@ -5,6 +5,12 @@ fun main(args: Array<String>){
     print("Enter integer from 1-15: ")
     var num:Int= readLine()!!.toInt()
 
+    while(num !in 1..15){
+        print("Invalid input. Enter integer from 1-15: ")
+        num= readLine()!!.toInt()
+    }
+
+
     val random = Random()
     println(List(num) {random.nextInt(0..9)})
 
